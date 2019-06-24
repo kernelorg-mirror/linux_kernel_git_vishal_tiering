3ee8678cf45aca3340aeab73d27fad14cd4b0ea8
Huang Ying <ying.huang@intel.com>
2019-06-24 09:03:55 +0800

    autonuma, hmem: Select hotter pages to promote to fast memory node
    In HMEM (Hierarchy Memory) system, to maximize the overall system
    performance, the hot pages should be put in the fast memory node while
    the cold pages should be put in the slow memory node.  In original
    autonuma implementation, we will try to promote almost all recently
    accessed pages, and use the LRU algorithm in page reclaiming to keep
    the hot pages in the fast memory node and demote the cold pages to the
    slow memory node.  The problem of this solution is that the cold pages
    with a low access frequency may be promoted then demoted too.  So that
    the memory bandwidth is wasted.  And if autonuma migration is
    rate-limited, the hot pages need to compete with the cold pages for
    the limited migration bandwidth.
    
    If we could select the hotter pages to promote to the fast memory node
    in the first place, then the wasted migration bandwidth would be
    reduced and the hot pages would be promoted more quickly.
    
    The patch "autonuma, hmem: Only promote page if accessed twice" in the
    series will prevent the really cold pages that are not accessed in the
    last scan period from being promoted.  But the scan period could be as
    long as tens seconds, so it doesn't work well enough on selecting the
    hotter pages.
    
    To identify the hotter pages, in this patch we implemented a method
    based on autonuma page table scanning and hint page fault as follow,
    
    - When a range of the page table is scanned in autonuma, the timestamp
      and the address range is recorded in a ring buffer in struct
      mm_struct.  So we have information of recent N scans.
    
    - When the autonuma hint page fault occurs, the fault address is
      searched in the ring buffer to get its scanning timestamp.  The hint
      page fault latency is defined as
    
        hint page fault timestamp - scan timestamp
    
      The access frequency of the hotter pages is higher.  So the
      probability for their hint page fault latency to be shorter is
      higher too.  So the hint page fault latency is a good estimation of
      the page heat.
    
    The remaining problem is how to determine the hot threshold.  It's not
    easy to be done automatically.  So we provide a sysctl knob:
    kernel.numa_balancing_hot_threshold_ms.  All pages with hint page
    fault latency < the threshold will be considered hot.  The system
    administrator can determine the hot threshold via various information,
    such as PMEM bandwidth limit, the average number of the pages pass the
    hot threshold, etc.  The default hot threshold is 1 second, which
    works well in our performance test.
    
    The downside of the patch is that the response time to the workload
    hot spot changing may be much longer.  For example,
    
    - A previous cold memory area becomes hot
    
    - The hint page fault will be triggered.  But the hint page fault
      latency may not be shorter than the hot threshold.  So the pages may
      not be promoted.
    
    - When the memory area is scanned again, maybe after a scan period,
      the hint page fault latency measured will be shorter than the hot
      threshold and the pages will be promoted.
    
    To mitigate this,
    
    - If there are enough free space in the fast memory node (> 2 * high
      watermark), the hot threshold will not be used, all pages will be
      promoted upon the hint page fault for fast response.
    
    - If fast response is more important for system performance, the
      administrator can set a higher hot threshold.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1053,6 +1053,9 @@ unsigned int sysctl_numa_balancing_scan_delay = 1000;
  */
 unsigned int sysctl_numa_balancing_rate_limit = 0;
 
+/* The page with hint page fault latency < threshold in ms is considered hot */
+unsigned int sysctl_numa_balancing_hot_threshold = 1000;
+
 struct numa_group {
 	refcount_t refcount;
 
@@ -1158,7 +1161,7 @@ static unsigned int task_scan_max(struct task_struct *p)
 
 void init_numa_balancing(unsigned long clone_flags, struct task_struct *p)
 {
-	int mm_users = 0;
+	int mm_users = 0, i;
 	struct mm_struct *mm = p->mm;
 
 	if (mm) {
@@ -1166,6 +1169,11 @@ void init_numa_balancing(unsigned long clone_flags, struct task_struct *p)
 		if (mm_users == 1) {
 			mm->numa_next_scan = jiffies + msecs_to_jiffies(sysctl_numa_balancing_scan_delay);
 			mm->numa_scan_seq = 0;
+			mm->numa_scan_idx = 0;
+			for (i = 0; i < NUMA_SCAN_NR_HIST; i++) {
+				mm->numa_scan_jiffies[i] = 0;
+				mm->numa_scan_starts[i] = 0;
+			}
 		}
 	}
 	p->node_stamp			= 0;
@@ -1422,7 +1430,8 @@ static bool pgdat_free_space_enough(struct pglist_data *pgdat)
 }
 
 bool should_numa_migrate_memory(struct task_struct *p, struct page * page,
-				int src_nid, int dst_cpu)
+				int src_nid, int dst_cpu, unsigned long addr,
+				int flags)
 {
 	struct numa_group *ng = p->numa_group;
 	int dst_nid = cpu_to_node(dst_cpu);
@@ -1431,13 +1440,48 @@ bool should_numa_migrate_memory(struct task_struct *p, struct page * page,
 	if (sysctl_numa_balancing_mode == NUMA_BALANCING_HMEM &&
 	    next_promotion_node(src_nid) == dst_nid) {
 		struct pglist_data *pgdat;
-		unsigned long now = jiffies, last_jiffies;
-		unsigned long try_migrate, rate_limit;
+		struct mm_struct *mm = p->mm;
+		unsigned long now = jiffies, last_jiffies, tj;
+		unsigned long try_migrate, rate_limit, threshold;
+		unsigned long start, end;
+		int i, j;
 
 		pgdat = NODE_DATA(dst_nid);
 		if (pgdat_free_space_enough(pgdat))
 			return true;
 
+		/* The page hasn't been accessed in the last scan period */
+		if (!(flags & TNF_YOUNG))
+			return false;
+
+		threshold = msecs_to_jiffies(
+			sysctl_numa_balancing_hot_threshold);
+		i = READ_ONCE(mm->numa_scan_idx);
+		i = i ? i - 1 : NUMA_SCAN_NR_HIST - 1;
+		/*
+		 * Paired with smp_wmb() in task_numa_work() to check
+		 * scan range buffer after get current index
+		 */
+		smp_rmb();
+		end = READ_ONCE(mm->numa_scan_offset);
+		start = READ_ONCE(mm->numa_scan_starts[i]);
+		if (start == end)
+			end = start + (MAX_SCAN_WINDOW << 22);
+		for (j = 0; j < NUMA_SCAN_NR_HIST; j++) {
+			tj = READ_ONCE(mm->numa_scan_jiffies[i]);
+			if ((long)(now - tj) > threshold)
+				return false;
+			start = READ_ONCE(mm->numa_scan_starts[i]);
+			/* Scan pass the end of address space */
+			if (end < start)
+				end = TASK_SIZE;
+			if (addr >= start && addr < end)
+				goto pass;
+			end = start;
+			i = i ? i - 1 : NUMA_SCAN_NR_HIST - 1;
+		}
+		/* The tracking window isn't large enough, assume page is hot */
+pass:
 		rate_limit = sysctl_numa_balancing_rate_limit << (20 - PAGE_SHIFT);
 		/* Default rate limit is high watermark size per second */
 		if (!rate_limit)
@@ -2495,7 +2539,7 @@ static void reset_ptenuma_scan(struct task_struct *p)
 	 * expensive, to avoid any form of compiler optimizations:
 	 */
 	WRITE_ONCE(p->mm->numa_scan_seq, READ_ONCE(p->mm->numa_scan_seq) + 1);
-	p->mm->numa_scan_offset = 0;
+	WRITE_ONCE(p->mm->numa_scan_offset, 0);
 }
 
 /*
@@ -2512,6 +2556,7 @@ void task_numa_work(struct callback_head *work)
 	unsigned long start, end;
 	unsigned long nr_pte_updates = 0;
 	long pages, virtpages;
+	int idx;
 
 	SCHED_WARN_ON(p != container_of(work, struct task_struct, numa_work));
 
@@ -2570,6 +2615,19 @@ void task_numa_work(struct callback_head *work)
 		start = 0;
 		vma = mm->mmap;
 	}
+	idx = mm->numa_scan_idx;
+	WRITE_ONCE(mm->numa_scan_starts[idx], start);
+	WRITE_ONCE(mm->numa_scan_jiffies[idx], jiffies);
+	/*
+	 * Paired with smp_rmb() in should_numa_migrate_memory() to
+	 * update scan range buffer index after update the buffer
+	 * contents.
+	 */
+	smp_wmb();
+	if (idx + 1 >= NUMA_SCAN_NR_HIST)
+		WRITE_ONCE(mm->numa_scan_idx, 0);
+	else
+		WRITE_ONCE(mm->numa_scan_idx, idx + 1);
 	for (; vma; vma = vma->vm_next) {
 		if (!vma_migratable(vma) || !vma_policy_mof(vma) ||
 			is_vm_hugetlb_page(vma) || (vma->vm_flags & VM_MIXEDMAP)) {
@@ -2597,6 +2655,7 @@ void task_numa_work(struct callback_head *work)
 			start = max(start, vma->vm_start);
 			end = ALIGN(start + (pages << PAGE_SHIFT), HPAGE_SIZE);
 			end = min(end, vma->vm_end);
+			WRITE_ONCE(mm->numa_scan_offset, end);
 			nr_pte_updates = change_prot_numa(vma, start, end);
 
 			/*
@@ -2626,9 +2685,7 @@ out:
 	 * would find the !migratable VMA on the next scan but not reset the
 	 * scanner to the start so check it now.
 	 */
-	if (vma)
-		mm->numa_scan_offset = start;
-	else
+	if (!vma)
 		reset_ptenuma_scan(p);
 	up_read(&mm->mmap_sem);
 
