8c29fb5c67db02fa4240500cf9b6e94aa6ffe0d8
Huang Ying <ying.huang@intel.com>
2019-07-12 09:52:58 +0800

    autonuma, memory tiering: Rate limit NUMA migration throughput
    In AutoNUMA memory tiering mode, the hot slow memory pages could be
    promoted to the fast memory node via AutoNUMA.  But this incurs some
    overhead too.  So that sometimes the workload performance may be hurt.
    To avoid too much disturbing to the workload in these situations, we
    should make it possible to rate limit the promotion throughput.
    
    So, in this patch, we implement a simple rate limit algorithm as
    follows.  The number of the candidate pages to be promoted to the fast
    memory node via AutoNUMA is counted, if the count exceeds the limit
    specified by the users, the AutoNUMA promotion will be stopped until
    the next second.
    
    Test the patch with the pmbench memory accessing benchmark with 80:20
    read/write ratio and normal access address distribution on a 2 socket
    Intel server with Optane DC Persistent Memory Model.  In the test, the
    page promotion throughput decreases 51.4% (from 213.0 MB/s to 103.6
    MB/s) with the patch, while the benchmark score decreases only 1.8%.
    
    A new sysctl knob kernel.numa_balancing_rate_limit_mbps is added for
    the users to specify the limit.
    
    TODO: Add ABI document for new sysctl knob.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mm@kvack.org

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -50,6 +50,12 @@ extern unsigned int sysctl_numa_balancing_scan_period_max;
 extern unsigned int sysctl_numa_balancing_scan_size;
 extern unsigned int sysctl_numa_balancing_hot_threshold;
 
+#ifdef CONFIG_NUMA_BALANCING
+extern unsigned int sysctl_numa_balancing_rate_limit;
+#else
+#define sysctl_numa_balancing_rate_limit	0
+#endif
+
 #ifdef CONFIG_SCHED_DEBUG
 extern __read_mostly unsigned int sysctl_sched_migration_cost;
 extern __read_mostly unsigned int sysctl_sched_nr_migrate;
