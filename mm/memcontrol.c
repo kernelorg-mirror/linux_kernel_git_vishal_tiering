ac8115ef83df774a4df6ff5a63bd05d657458a2a
Huang Ying <ying.huang@intel.com>
2020-07-28 15:05:52 +0800

    mm: Make some global page reclaim related vmstat events per-node
    If the workloads of the system isn't distributed evenly among the NUMA
    nodes, the page reclaiming behavior may be different in different NUMA
    nodes.  For example, in a memory tiering system with DRAM and PMEM,
    the page reclaiming in the DRAM node may be used to demote the cold
    pages to the PMEM node.  To understand the system behavior more
    clearly, sometimes, we need to check the per-node page reclaiming
    events.  So in this patch, we make some global page reclaim related
    vmstat events per-node to help this.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1546,21 +1546,21 @@ static char *memory_stat_format(struct mem_cgroup *memcg)
 		       memcg_page_state(memcg, WORKINGSET_NODERECLAIM));
 
 	seq_buf_printf(&s, "%s %lu\n",  vm_event_name(PGREFILL),
-		       memcg_events(memcg, PGREFILL));
+		       memcg_page_state(memcg, PGREFILL));
 	seq_buf_printf(&s, "pgscan %lu\n",
-		       memcg_events(memcg, PGSCAN_KSWAPD) +
-		       memcg_events(memcg, PGSCAN_DIRECT));
+		       memcg_page_state(memcg, PGSCAN_KSWAPD) +
+		       memcg_page_state(memcg, PGSCAN_DIRECT));
 	seq_buf_printf(&s, "pgsteal %lu\n",
-		       memcg_events(memcg, PGSTEAL_KSWAPD) +
-		       memcg_events(memcg, PGSTEAL_DIRECT));
+		       memcg_page_state(memcg, PGSTEAL_KSWAPD) +
+		       memcg_page_state(memcg, PGSTEAL_DIRECT));
 	seq_buf_printf(&s, "%s %lu\n", vm_event_name(PGACTIVATE),
-		       memcg_events(memcg, PGACTIVATE));
+		       memcg_page_state(memcg, PGACTIVATE));
 	seq_buf_printf(&s, "%s %lu\n", vm_event_name(PGDEACTIVATE),
-		       memcg_events(memcg, PGDEACTIVATE));
+		       memcg_page_state(memcg, PGDEACTIVATE));
 	seq_buf_printf(&s, "%s %lu\n", vm_event_name(PGLAZYFREE),
 		       memcg_events(memcg, PGLAZYFREE));
 	seq_buf_printf(&s, "%s %lu\n", vm_event_name(PGLAZYFREED),
-		       memcg_events(memcg, PGLAZYFREED));
+		       memcg_page_state(memcg, PGLAZYFREED));
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	seq_buf_printf(&s, "%s %lu\n", vm_event_name(THP_FAULT_ALLOC),
--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1178,11 +1178,11 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 			    &nr_succeeded);
 
 	if (current_is_kswapd())
-		__count_vm_events(PGDEMOTE_KSWAPD, nr_succeeded);
+		__mod_node_page_state(pgdat, PGDEMOTE_KSWAPD, nr_succeeded);
 	else
-		__count_vm_events(PGDEMOTE_DIRECT, nr_succeeded);
+		__mod_node_page_state(pgdat, PGDEMOTE_DIRECT, nr_succeeded);
 	if (file_lru)
-		__count_vm_events(PGDEMOTE_FILE, nr_succeeded);
+		__mod_node_page_state(pgdat, PGDEMOTE_FILE, nr_succeeded);
 
 	return nr_succeeded;
 }
@@ -1562,8 +1562,8 @@ retry:
 				goto keep_locked;
 			}
 
-			count_vm_event(PGLAZYFREED);
-			count_memcg_page_event(page, PGLAZYFREED);
+			inc_node_page_state(page, PGLAZYFREED);
+			inc_memcg_page_state(page, PGLAZYFREED);
 		} else if (!mapping || !__remove_mapping(mapping, page, true,
 							 sc->target_mem_cgroup))
 			goto keep_locked;
@@ -1605,7 +1605,7 @@ activate_locked:
 			int type = page_is_file_lru(page);
 			SetPageActive(page);
 			stat->nr_activate[type] += nr_pages;
-			count_memcg_page_event(page, PGACTIVATE);
+			inc_memcg_page_state(page, PGACTIVATE);
 		}
 keep_locked:
 		unlock_page(page);
@@ -1632,7 +1632,7 @@ keep:
 	free_unref_page_list(&free_pages);
 
 	list_splice(&ret_pages, page_list);
-	count_vm_events(PGACTIVATE, pgactivate);
+	mod_node_page_state(pgdat, PGACTIVATE, pgactivate);
 
 	return nr_reclaimed;
 }
@@ -2093,8 +2093,8 @@ shrink_inactive_list(unsigned long nr_to_scan, struct lruvec *lruvec,
 	__mod_node_page_state(pgdat, NR_ISOLATED_ANON + file, nr_taken);
 	item = current_is_kswapd() ? PGSCAN_KSWAPD : PGSCAN_DIRECT;
 	if (!cgroup_reclaim(sc))
-		__count_vm_events(item, nr_scanned);
-	__count_memcg_events(lruvec_memcg(lruvec), item, nr_scanned);
+		__mod_node_page_state(pgdat, item, nr_scanned);
+	__mod_memcg_state(lruvec_memcg(lruvec), item, nr_scanned);
 	__count_vm_events(PGSCAN_ANON + file, nr_scanned);
 
 	spin_unlock_irq(&pgdat->lru_lock);
@@ -2113,8 +2113,8 @@ shrink_inactive_list(unsigned long nr_to_scan, struct lruvec *lruvec,
 	lru_note_cost(lruvec, file, stat.nr_pageout);
 	item = current_is_kswapd() ? PGSTEAL_KSWAPD : PGSTEAL_DIRECT;
 	if (!cgroup_reclaim(sc))
-		__count_vm_events(item, nr_reclaimed);
-	__count_memcg_events(lruvec_memcg(lruvec), item, nr_reclaimed);
+		__mod_node_page_state(pgdat, item, nr_reclaimed);
+	__mod_memcg_state(lruvec_memcg(lruvec), item, nr_reclaimed);
 	__count_vm_events(PGSTEAL_ANON + file, nr_reclaimed);
 
 	spin_unlock_irq(&pgdat->lru_lock);
@@ -2177,8 +2177,8 @@ static void shrink_active_list(unsigned long nr_to_scan,
 	__mod_node_page_state(pgdat, NR_ISOLATED_ANON + file, nr_taken);
 
 	if (!cgroup_reclaim(sc))
-		__count_vm_events(PGREFILL, nr_scanned);
-	__count_memcg_events(lruvec_memcg(lruvec), PGREFILL, nr_scanned);
+		__mod_node_page_state(pgdat, PGREFILL, nr_scanned);
+	__mod_memcg_state(lruvec_memcg(lruvec), PGREFILL, nr_scanned);
 
 	spin_unlock_irq(&pgdat->lru_lock);
 
@@ -2233,8 +2233,8 @@ static void shrink_active_list(unsigned long nr_to_scan,
 	/* Keep all free pages in l_active list */
 	list_splice(&l_inactive, &l_active);
 
-	__count_vm_events(PGDEACTIVATE, nr_deactivate);
-	__count_memcg_events(lruvec_memcg(lruvec), PGDEACTIVATE, nr_deactivate);
+	__mod_node_page_state(pgdat, PGDEACTIVATE, nr_deactivate);
+	__mod_memcg_state(lruvec_memcg(lruvec), PGDEACTIVATE, nr_deactivate);
 
 	__mod_node_page_state(pgdat, NR_ISOLATED_ANON + file, -nr_taken);
 	spin_unlock_irq(&pgdat->lru_lock);
@@ -3722,7 +3722,7 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int highest_zoneidx)
 	psi_memstall_enter(&pflags);
 	__fs_reclaim_acquire();
 
-	count_vm_event(PAGEOUTRUN);
+	inc_node_state(pgdat, PAGEOUTRUN);
 
 	/*
 	 * Account for the reclaim boost. Note that the zone boost is left in
@@ -4017,9 +4017,9 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 		set_pgdat_percpu_threshold(pgdat, calculate_pressure_threshold);
 	} else {
 		if (remaining)
-			count_vm_event(KSWAPD_LOW_WMARK_HIT_QUICKLY);
+			inc_node_state(pgdat, KSWAPD_LOW_WMARK_HIT_QUICKLY);
 		else
-			count_vm_event(KSWAPD_HIGH_WMARK_HIT_QUICKLY);
+			inc_node_state(pgdat, KSWAPD_HIGH_WMARK_HIT_QUICKLY);
 	}
 	finish_wait(&pgdat->kswapd_wait, &wait);
 }
