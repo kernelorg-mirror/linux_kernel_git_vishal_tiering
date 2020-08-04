5e63461011a6a437ca922af824806c95be7ffdc2
Tim Chen <tim.c.chen@linux.intel.com>
2020-08-04 11:08:24 -0700

    mm: Define top tier memory node mask
    The fast/expensive memory lives in the top tier of the memory
    hierachy and it is a precious resource that needs to be accounted and
    managed on a memory cgroup basis.
    
    Define the top tier memory as the memory nodes that don't have demotion
    paths into it from higher tier memory.

--- mm/migrate.c
+++ mm/migrate.c
@@ -3439,6 +3439,7 @@ static int establish_migrate_target(int node, nodemask_t *used)
 		return NUMA_NO_NODE;
 
 	node_demotion[node] = migration_target;
+	node_clear_state(migration_target, N_TOPTIER);
 
 	return migration_target;
 }
