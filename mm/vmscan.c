4159d3482a8e2b82d268b629053c1908a30c5291
Huang Ying <ying.huang@intel.com>
2020-09-09 10:15:47 +0800

    memory tiering: add file pages demotion counter
    To distinguish the file pages demotion from the anonymous pages
    demotion.  This can be used to check the balance between the
    file/anonymous pages demotion.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1146,10 +1146,12 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 	int target_nid = next_demotion_node(pgdat->node_id);
 	unsigned int nr_succeeded = 0;
 	int err;
+	bool file_lru;
 
 	if (list_empty(demote_pages))
 		return 0;
 
+	file_lru = page_is_file_lru(lru_to_page(demote_pages));
 	/* Demotion ignores all cpuset and mempolicy settings */
 	err = migrate_pages(demote_pages, alloc_demote_page, NULL,
 			    target_nid, MIGRATE_ASYNC, MR_DEMOTION,
@@ -1159,6 +1161,8 @@ static unsigned int demote_page_list(struct list_head *demote_pages,
 		__count_vm_events(PGDEMOTE_KSWAPD, nr_succeeded);
 	else
 		__count_vm_events(PGDEMOTE_DIRECT, nr_succeeded);
+	if (file_lru)
+		__count_vm_events(PGDEMOTE_FILE, nr_succeeded);
 
 	return nr_succeeded;
 }
--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1252,6 +1252,7 @@ const char * const vmstat_text[] = {
 	"pgsteal_direct",
 	"pgdemote_kswapd",
 	"pgdemote_direct",
+	"pgdemote_file",
 	"pgscan_kswapd",
 	"pgscan_direct",
 	"pgscan_direct_throttle",
