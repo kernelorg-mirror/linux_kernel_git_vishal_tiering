365a46729a7ec0abba3bd410e4718fec6e5e1f1d
Huang Ying <ying.huang@intel.com>
2020-10-10 11:31:18 +0800

    fix stack guard error

--- mm/migrate.c
+++ mm/migrate.c
@@ -1525,7 +1525,7 @@ retry:
 			case MIGRATEPAGE_SUCCESS:
 				if (is_thp) {
 					nr_thp_succeeded++;
-					nr_succeeded += nr_subpages;
+					*nr_succeeded += nr_subpages;
 					break;
 				}
 				(*nr_succeeded)++;
