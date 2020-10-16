aa3fa0d68784bf9ad0d36870833a6232438f2cd6
Huang Ying <ying.huang@intel.com>
2020-10-16 13:40:47 +0800

    mem_cgroup: add some page reclaim/promotion stats

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4052,6 +4052,13 @@ static const unsigned int memcg1_stats[] = {
 	NR_FILE_MAPPED,
 	NR_FILE_DIRTY,
 	NR_WRITEBACK,
+	PGSTEAL_KSWAPD,
+	PGSTEAL_DIRECT,
+	PGDEMOTE_KSWAPD,
+	PGDEMOTE_DIRECT,
+#ifdef CONFIG_NUMA_BALANCING
+	PGPROMOTE_SUCCESS,	/* promote successfully */
+#endif
 	MEMCG_SWAP,
 };
 
@@ -4065,6 +4072,13 @@ static const char *const memcg1_stat_names[] = {
 	"mapped_file",
 	"dirty",
 	"writeback",
+	"steal_kswapd",
+	"steal_direct",
+	"demote_kswapd",
+	"demote_direct",
+#ifdef CONFIG_NUMA_BALANCING
+	"promote_success",
+#endif
 	"swap",
 };
 
