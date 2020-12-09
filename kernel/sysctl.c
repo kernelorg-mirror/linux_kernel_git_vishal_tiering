d0cb97a6f327b4c0f743d9c565fc9c69ba049419
Huang Ying <ying.huang@intel.com>
2020-12-09 13:55:07 +0800

    numa balancing: Add sysctl numa_balancing_write_bias
    The read/write performance of the slow memory may be different, so it may be
    better to use different threshold for memory read and write.  This may be
    workload specific too.  So add a sysctl knob to make it possible to tune this.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1770,6 +1770,14 @@ static struct ctl_table kern_table[] = {
 		.extra1		= SYSCTL_ZERO,
 	},
 	{
+		.procname	= "numa_balancing_write_bias",
+		.data		= &sysctl_numa_balancing_write_bias,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ONE,
+	},
+	{
 		.procname	= "numa_balancing",
 		.data		= &sysctl_numa_balancing_mode,
 		.maxlen		= sizeof(int),
