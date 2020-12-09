c9cc4dece9eef9b1f431d8abf5bd6c2fb4aa8e72
Huang Ying <ying.huang@intel.com>
2020-12-09 16:41:41 +0800

    dbg: mark NUMA balancing entry as noinline
    To help trace the functions.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2329,7 +2329,7 @@ static inline bool is_shared_exec_page(struct vm_area_struct *vma,
  * node. Caller is expected to have an elevated reference count on
  * the page that will be dropped by this function before returning.
  */
-int migrate_misplaced_page(struct page *page, struct vm_area_struct *vma,
+int noinline migrate_misplaced_page(struct page *page, struct vm_area_struct *vma,
 			   int node)
 {
 	pg_data_t *pgdat = NODE_DATA(node);
@@ -2394,7 +2394,7 @@ out:
  * Migrates a THP to a given target node. page must be locked and is unlocked
  * before returning.
  */
-int migrate_misplaced_transhuge_page(struct mm_struct *mm,
+int noinline migrate_misplaced_transhuge_page(struct mm_struct *mm,
 				struct vm_area_struct *vma,
 				pmd_t *pmd, pmd_t entry,
 				unsigned long address,
